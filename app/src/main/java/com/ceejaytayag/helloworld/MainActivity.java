4a6969ae9528b6bdcec9d87a06aba299525432ac
ceejay-tayag <tayagfrahserjay@gmail.com>
2021-04-04 20:33:14 +0800

    Hello-World first commit

---
+++ app/src/main/java/com/ceejaytayag/helloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package com.ceejaytayag.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //main-method
+
+
+    }
+
+    //additional methods
+
+
+
+
+
+
+
+
+}
