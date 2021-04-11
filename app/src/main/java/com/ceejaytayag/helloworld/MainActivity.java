ddd06070800e4e79cbaa5da0381f06394679e9e9
ceejay-tayag <tayagfrahserjay@gmail.com>
2021-04-11 17:50:39 +0800

    Your upload changes

--- app/src/main/java/com/ceejaytayag/helloworld/MainActivity.java
+++ app/src/main/java/com/ceejaytayag/helloworld/MainActivity.java
@@ -2,27 +2,52 @@ package com.ceejaytayag.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //main-method
+        Button button1 = findViewById(R.id.form);
+        Button button2 = findViewById(R.id.data);
+        Button button3 = findViewById(R.id.resource);
+
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
 
 
     }
 
-    //additional methods
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.form:
+                goToUrl ( "https://forms.gle/MuS3a7FtBW5vCZC37");
+                break;
+            case R.id.data:
+                goToUrl ( "https://docs.google.com/spreadsheets/d/1aYUaxWu8q2OVu7_7XBOdnbpBrvPspfwQE5SBf6DgQ2A/edit#gid=536483911");
+                break;
+            case R.id.resource:
+                goToUrl ( "https://www.businesslist.ph/location/pasig?amp=1");
+                break;
 
+        }
+    }
 
-
-
-
-
-
-
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
 }
